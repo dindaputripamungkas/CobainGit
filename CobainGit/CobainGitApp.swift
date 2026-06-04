531779f479818eaa647bce3049eaf3b116708237
Dinda Puutri Pamungkas <dindappamungkas@gmail.com>
2026-06-04 09:00:47 +0700

    Initial Commit

---
+++ CobainGit/CobainGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CobainGitApp.swift
+//  CobainGit
+//
+//  Created by Dinda Putri Pamungkas  on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CobainGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
