c3e6437ddd6b777466c61f5917240b33f0b1d852
Dinda Puutri Pamungkas <dindappamungkas@gmail.com>
2026-06-04 11:07:44 +0700

    commit 2

---
+++ CobainGit/feat:pagebaru.swift
@@ -0,0 +1,18 @@
+//
+//  feat:pagebaru.swift
+//  CobainGit
+//
+//  Created by Dinda Putri Pamungkas  on 04/06/26.
+//
+
+import SwiftUI
+
+struct feat_pagebaru: View {
+    var body: some View {
+        Text("Hello, Dinda Putri Pamungkas!")
+    }
+}
+
+#Preview {
+    feat_pagebaru()
+}
