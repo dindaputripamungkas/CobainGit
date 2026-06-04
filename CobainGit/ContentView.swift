de2b3811218f713b9b76447efcb134fc8428a879
Dinda Puutri Pamungkas <dindappamungkas@gmail.com>
2026-06-04 09:48:50 +0700

    initial commit add text and create new file

--- CobainGit/ContentView.swift
+++ CobainGit/ContentView.swift
@@ -14,6 +14,10 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("This is my first SwiftUI app")
+            Button("Click me") {
+                
+            }
         }
         .padding()
     }
---
+++ CobainGit/Page2.swift
@@ -0,0 +1,18 @@
+//
+//  Page2.swift
+//  CobainGit
+//
+//  Created by Dinda Putri Pamungkas  on 04/06/26.
+//
+
+import SwiftUI
+
+struct Page2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Page2()
+}
