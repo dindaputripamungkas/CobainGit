1d5151b3eeb712196ffa34e57f940dcbb8bffb59
Zulkifli <zulkiflibasri8@gmail.com>
2026-06-04 11:06:14 +0700

    add text

---
+++ CobainGit/OnBoarding1.swift
@@ -0,0 +1,32 @@
+//
+//  OnBoarding1.swift
+//  CobainGit
+//
+//  Created by Zulkifli on 04/06/26.
+//
+
+import SwiftUI
+
+struct OnBoarding1: View {
+    var body: some View {
+        ZStack{
+            
+        }
+        VStack{
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+            Text("Create Your Own Route")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(Color.blue)
+            
+          
+            
+        }
+    
+    }
+}
+
+#Preview {
+    OnBoarding1()
+}
